7235d66102f6fb1d44a6e543f9ef77cc38533528
DimTrigkakis <trigkakd@oregonstate.edu>
2016-10-23 02:43:48 -0700

    Final Engine

---
+++ empty.py
@@ -0,0 +1,13 @@
+import gate
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
+
+myGate = gate.Gate()
+menu = None
+keys = None
+textures = None
+
+myGate.foundation(menu,keys,textures)
+
+
