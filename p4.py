977de5d51b1a34742ff06d4a4d76e6ae5decd5f3
simone <jim.tr@hotmail.com>
2016-11-23 15:18:19 -0800

    Final

--- p4.py
+++ p4.py
@@ -10,10 +10,10 @@ import numpy
 import shaders
 
 myGate = gate.Gate()
-heightmap = generator.heightmaps()
+heightmap, water_height = generator.heightmaps()
 
-#t = Thread(target=myGate.audio, args=())
-#t.start()
+t = Thread(target=myGate.audio, args=())
+t.start()
 
 character = [0,0]
 
@@ -24,15 +24,16 @@ right_step = 0
 mouse_x = 0
 mouse_y = 0
 
-
 once = True
 scale = 16
 
 waveTime = 0.0
 program = None
+program2 = None
+
 def Animate(timeval):
 	
-	global forward_step, right_step, backward_step,mouse_x,mouse_y, once,scale, waveTime, program, text
+	global forward_step, right_step, backward_step,mouse_x,mouse_y, once,scale, waveTime, program, program2, text
 
 	random_creation()
 	if (once):
@@ -42,7 +43,7 @@ def Animate(timeval):
 		glFogi(GL_FOG_MODE, GL_EXP)
 		glFogfv(GL_FOG_COLOR, fogColor)
 		glFogf(GL_FOG_DENSITY, 0.01)
-		program = shaders.init()	
+		program, program2 = shaders.init()	
 		
 
 		myGate.camera.center = [generator.m/2,30.0,generator.m/2]
@@ -76,22 +77,27 @@ def Animate(timeval):
 	l = myGate.find_gobject("player_point_light").location
 	myGate.find_gobject("player_point_light").location = (0.99*l[0]+0.01*(x+5),0.9*l[1]+0.1*(y-8+math.cos(waveTime*400)),z)
 	myGate.find_gobject("sky").location = (x+5,y-10,z)
+	myGate.find_gobject("snow").location = (x+5,y-10,z)
 
 	r = 10
 	l = myGate.camera.look
 	c = myGate.camera.center
 
-
-	d1 = abs(heightmap[int(c[0])][int(c[2])]+2 - l[1])
-	d2 = abs(heightmap[int(c[0])][int(c[2])]+2 - c[1])
-
-	if d1 > 1:
-		d1 = 1
-	if d2 > 1: 
-		d2 = 1
+	try:
+		d1 = abs(heightmap[int(c[0])][int(c[2])]+2 - l[1])
+		d2 = abs(heightmap[int(c[0])][int(c[2])]+2 - c[1])
+		if d1 > 1:
+			d1 = 1
+		if d2 > 1: 
+			d2 = 1
 	
-	myGate.camera.look[1] = l[1]*0.9+0.1*(heightmap[int(c[0])][int(c[2])]+8)
-	myGate.camera.center[1] = c[1]*0.9 + 0.1*(heightmap[int(c[0])][int(c[2])]+8)
+		myGate.camera.look[1] = l[1]*0.9+0.1*(max(water_height+0.75,heightmap[int(c[0])][int(c[2])]+8))
+		myGate.camera.center[1] = c[1]*0.9 + 0.1*(max(water_height+0.75,heightmap[int(c[0])][int(c[2])]+8))
+	except:
+	
+		myGate.camera.look[1] = l[1]*0.9+0.1*(max(water_height+0.75,8))
+		myGate.camera.center[1] = c[1]*0.9 + 0.1*(max(water_height+0.75,8))
+
 
 	alpha = 0
 	if forward_step:
@@ -168,7 +174,67 @@ def dist(center, (i,h,j)):
 	return math.sqrt((i-x)*(i-x)+(j-z)*(j-z))
 
 	
+def draw_snow(gid,tid,name):
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE)
+	glEnable(GL_BLEND)
+	glDisable(GL_DEPTH_TEST)
+	if (once != True):
+		shaders.enable(2)
+		loc=glGetUniformLocation(program2,"waveTime")
+		glUniform1f(loc,100*waveTime)
+
+	glRotatef(waveTime,0,1,0)
+	glutSolidSphere(200,10,10)
+	#glutSolidSphere(60,10,10)
+	#glutSolidSphere(30,10,10)
+	glDisable(GL_BLEND)
+	glEnable(GL_DEPTH_TEST)
+	shaders.disable()
 	
+def draw_water(gid,tid,name):
+	global scale, water_height, program, program2, waveTime, once
+	
+	if (once != True):
+		shaders.enable(1)
+		loc=glGetUniformLocation(program,"waveTime")
+		glUniform1f(loc,10*waveTime)
+
+	myGate.set_texture(tid)
+	draw_size =15
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [60/256.0, 60/256.0, 60/256.0, 1.0])
+   	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, [0, 0, 0, 0])
+    	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, [0.0])
+	glShadeModel( GL_SMOOTH )
+	glBegin(GL_TRIANGLES)
+	glNormal3f(0,1,0)
+	for ii in range(int(myGate.camera.center[0]/scale-draw_size),int(myGate.camera.center[0]/scale+draw_size)):
+		for jj in range(int(myGate.camera.center[2]/scale-draw_size),int(myGate.camera.center[2]/scale+draw_size)):
+			i = scale*ii
+			j = scale*jj
+			if (i > 0 and j > 0 and i < generator.m-scale and j < generator.m-scale):
+				
+				a1 = 3.0
+				a2 = 30
+				a3 = a2/1.0
+				i1 = (i*a1 % a2)/a3
+				j1 = (j*a1 % a2)/a3
+				i2 = ((i+scale)*a1 % a2)/a3
+				j2 = ((j+scale)*a1 % a2)/a3
+				glTexCoord2f(i1,j1)
+				glVertex3f(i,water_height,j)
+				glTexCoord2f(i2,j1)
+				glVertex3f(i+scale,water_height,j)
+				glTexCoord2f(i1,j2)
+				glVertex3f(i,water_height,j+scale)
+
+				glVertex3f(i,water_height,j+scale)
+				glTexCoord2f(i2,j1)
+				glVertex3f(i+scale,water_height,j)
+				glTexCoord2f(i2,j2)
+				glVertex3f(i+scale,water_height,j+scale)
+	glEnd()
+	shaders.disable()
+
 def draw_heightmap(gid,tid,name):
 	global scale
 	myGate.set_texture(tid)
@@ -208,12 +274,14 @@ def draw_heightmap(gid,tid,name):
 
 	glEnd()
 
+
+
 def draw_sky(gid,tid,name):
 	
-	global lights_on, program, waveTime, once
+	global lights_on, program, program2, waveTime, once
 	
 	if (once != True):
-		shaders.enable()
+		shaders.enable(1)
 		loc=glGetUniformLocation(program,"waveTime")
 		glUniform1f(loc,waveTime)
 	if  not lights_on:
@@ -411,7 +479,7 @@ def random_creation():
 			rx = +500*random.random()-250
 			ry = +500*random.random()-250
 		rz = 10
-		rt = random.randint(0,10)+2
+		rt = random.randint(0,10)+3
 		if (random_canvas == None) or (dist(myGate.camera.center,(random_canvas.location[0],random_canvas.location[1],random_canvas.location[2])) > 250):
 			
 			if random_canvas != None:
@@ -436,11 +504,13 @@ def create_entities():
 menu = []
 gui = [draw_text]
 keys = [("Key",'w',forward),("KeyUp",'w',forward_stop),("Key",'d',right_side),("KeyUp",'d',right_side_stop),("Key",'s',backward),("KeyUp",'s',backward_stop),("Key",'a',left_side),("Key",'e',read_text),("KeyUp",'a',left_side_stop),("Mouse","None",mouse_move),("MouseLeft","None",forward),("MouseMiddle","None",read_text),("MouseRight","None",backward),("MouseLeftUp","None",forward_stop),("MouseRightUp","None",backward_stop)]
-textures = ["marble.jpg","starnight.jpg"]
+textures = ["grass.jpg","starnight.jpg","water.jpg"]
 
 myGate.add_gobject(draw_object_node,name="player_point_light")
 myGate.add_gobject(draw_heightmap,tid=0,name="heightmap")
+myGate.add_gobject(draw_water,tid=2,name="water")
 myGate.add_gobject(draw_sky,tid=1,name="sky")
+myGate.add_gobject(draw_snow,tid=-1,name="snow")
 light0 = gate.Data(location=(0,10,0),type="Point",color=(0.9,1,0.9),parent=myGate.find_gobject("player_point_light"))
 lights = create_entities()
 for t in range(10):
@@ -452,10 +522,5 @@ for light in lights:
 
 myGate.foundation(menu,keys,textures,gui,true_lights)
 
-# TO DO LIST
-
-
-# script to create canvas model and place texture inside it
-# script to create interaction nodes
 
 
--- shaders.py
+++ shaders.py
@@ -6,6 +6,7 @@ from OpenGL.GL import *
 # Create and Compile a shader
 # but fail with a meaningful message if something goes wrong
 program = None
+program2 = None
 
 def createAndCompileShader(type,source):
     shader=glCreateShader(type)
@@ -22,7 +23,7 @@ def createAndCompileShader(type,source):
     return shader
 
 def init():
-	global program
+	global program, program2
 
 	vertex_shader=createAndCompileShader(GL_VERTEX_SHADER,"""
 	  
@@ -76,8 +77,190 @@ def init():
 	   vec4 color2 = texture2D(waveTextureIdRef, vec2(vTexCoord));
 	   
 
-	   gl_FragColor = 0.6 * vec4(color1 + color2) * vec4(0.0, 1.0, 1.0, 0.50); 
+	   gl_FragColor = 0.6 * vec4(color1 + color2) * vec4(0.1, 1.00, 1.0, 0.50); 
 	}
+	""");
+
+	
+	fragment_shader_snow=createAndCompileShader(GL_FRAGMENT_SHADER,"""
+
+	// By @paulofalcao
+	//
+	// Merry Christmas! :)
+	//
+	//
+	// Some GLSL compilers/drivers can't optimize the exit from the loop on break
+	// and always run all iterations.
+	//
+	// Works very nice on my MacBook Pro with NVIDIA 320M on MacOSX 10.7 / Chrome
+	// But runs very very slow on a Macbook Pro NVIDIA 320M on Win7 / Chrome
+
+
+	#ifdef GL_ES
+	precision highp float;
+	#endif
+
+	uniform float waveTime;
+
+	//Util Start
+
+	float PI=3.14159265;
+
+
+	vec3 sim(vec3 p,float s){
+	   vec3 ret=p;
+	   ret=p+s/2.0;
+	   ret=fract(ret/s)*s-s/2.0;
+	   return ret;
+	}
+
+	vec2 rot(vec2 p,float r){
+	   vec2 ret;
+	   ret.x=p.x*cos(r)-p.y*sin(r);
+	   ret.y=p.x*sin(r)+p.y*cos(r);
+	   return ret;
+	}
+
+	vec2 rotsim(vec2 p,float s){
+	   vec2 ret=p;
+	   ret=rot(p,-PI/(s*2.0));
+	   ret=rot(p,floor(atan(ret.x,ret.y)/PI*s)*(PI/s));
+	   return ret;
+	}
+
+	float rnd(vec2 v){
+	  return sin((sin(((v.y-1453.0)/(v.x+1229.0))*23232.124))*16283.223)*0.5+0.5; 
+	}
+
+	float noise(vec2 v){
+	  vec2 v1=floor(v);
+	  vec2 v2=smoothstep(0.0,1.0,fract(v));
+	  float n00=rnd(v1);
+	  float n01=rnd(v1+vec2(0,1));
+	  float n10=rnd(v1+vec2(1,0));
+	  float n11=rnd(v1+vec2(1,1));
+	  return mix(mix(n00,n01,v2.y),mix(n10,n11,v2.y),v2.x);
+	}
+
+	//Util End
+
+	 
+	//Scene Start
+	 
+
+	//Snow
+	float makeshowflake(vec3 p){
+	  return length(p)-0.03;
+	}
+
+	float makeShow(vec3 p,float tx,float ty,float tz){
+	  p.y=p.y+waveTime*tx;
+	  p.x=p.x+waveTime*ty;
+	  p.z=p.z+waveTime*tz;
+	  p=sim(p,4.0);
+	  return makeshowflake(p);
+	}
+
+	vec2 obj1(vec3 p){
+	  float f=makeShow(p,1.11, 1.03, 1.38);
+	  f=min(f,makeShow(p,1.72, 0.74, 1.06));
+	  f=min(f,makeShow(p,1.93, 0.75, 1.35));
+	  f=min(f,makeShow(p,1.54, 0.94, 1.72));
+	  f=min(f,makeShow(p,1.35, 1.33, 1.13));
+	  f=min(f,makeShow(p,1.55, 0.23, 1.16));
+	  f=min(f,makeShow(p,1.25, 0.41, 1.04));
+	  f=min(f,makeShow(p,1.49, 0.29, 1.31));
+	  f=min(f,makeShow(p,1.31, 1.31, 1.13));  
+	  return vec2(f,1.0);
+	}
+	 
+	vec3 obj1_c(vec3 p){
+	    return vec3(1,1,1);
+	}
+
+
+	vec3 obj2_c(vec3 p){
+	  return vec3(1.0,0.5,0.2);
+	}
+	 
+	//Objects union
+	vec2 inObj(vec3 p){
+	  return obj1(p);
+	}
+	 
+	//Scene End
+	 
+	void main(void){
+	  vec2 resolution = vec2(1000,1000);
+	  vec2 vPos=-1.0+2.0*gl_FragCoord.xy/resolution.xy;
+	 
+	  //Camera animation
+	  vec3 vuv=normalize(vec3(sin(waveTime)*0.5,1,2));
+	  vec3 vrp=vec3(0,cos(waveTime*0.5)+2.5,0);
+	  vec3 prp=vec3(sin(waveTime*0.5)*(sin(waveTime*0.39)*2.0+3.5),sin(waveTime*0.5)+3.5,cos(waveTime*0.5)*(cos(waveTime*0.45)*2.0+3.5));
+	  float vpd=1.5;  
+	 
+	  //Camera setup
+	  vec3 vpn=normalize(vrp-prp);
+	  vec3 u=normalize(cross(vuv,vpn));
+	  vec3 v=cross(vpn,u);
+	  vec3 scrCoord=prp+vpn*vpd+vPos.x*u*resolution.x/resolution.y+vPos.y*v;
+	  vec3 scp=normalize(scrCoord-prp);
+	 
+	  //lights are 2d, no raymarching
+	  mat4 cm=mat4(
+	    u.y,   u.y,   u.z,   -dot(u,prp),
+	    v.x,   v.y,   v.z,   -dot(v,prp),
+	    vpn.x, vpn.y, vpn.z, -dot(vpn,prp),
+	    0.0,   0.0,   0.0,   1.0);
+	 
+	  vec4 pc=vec4(0,0,0,0);
+	  const float maxl=80.0;
+	  for(float i=0.0;i<maxl;i++){
+	  vec4 pt=vec4(
+	    sin(i*PI*2.0*7.0/maxl)*2.0*(1.0-i/maxl),
+	    i/maxl*4.0,
+	    cos(i*PI*2.0*7.0/maxl)*2.0*(1.0-i/maxl),
+	    1.0);
+	  pt=pt*cm;
+	  vec2 xy=(pt/(-pt.z/vpd)).xy+vPos*vec2(resolution.x/resolution.y,1.0);
+	  float c;
+	  c=0.4/length(xy);
+	  pc+=vec4(
+		  (sin(i*5.0+waveTime*10.0)*0.5+0.5)*c,
+		  (cos(i*3.0+waveTime*8.0)*0.5+0.5)*c,
+		  (sin(i*1.0+waveTime*9.0)*0.5+0.5)*c,0.0);
+	  }
+	  pc=pc/maxl;
+	  pc=smoothstep(0.0,1.0,pc);
+	  pc=vec4(pc.x,pc.y,pc.z,1.0);
+	  
+	  //Raymarching
+	  const vec3 e=vec3(0.1,0,0);
+	  const float maxd=15.0; //Max depth
+	 
+	  vec2 s=vec2(0.9,0.0);
+	  vec3 c,p,n;
+	 
+	  float f=1.0;
+	  for(int i=0;i<30;i++){
+	    if (abs(s.x)<.001||f>maxd) break;
+	    f+=s.x;
+	    p=prp+scp*f;
+	    s=inObj(p);
+	  }
+	  
+	  if (f<maxd){
+	      c=obj1_c(p);
+		c[0] += 0.5;
+		c[1] += 0.5;
+		gl_FragColor=vec4(c*max(1.0-f*.08,0.0),0.1);
+	      
+	  }
+	  else gl_FragColor=vec4(0,0,0,0.5); //background color
+	}
+
+
 	""");
 
 	# build shader program
@@ -86,15 +269,22 @@ def init():
 	glAttachShader(program,vertex_shader)
 	glAttachShader(program,fragment_shader)
 	glLinkProgram(program)
+	program2=glCreateProgram()
+	glAttachShader(program2,vertex_shader)
+	glAttachShader(program2,fragment_shader_snow)
+	glLinkProgram(program2)
 
 	# try to activate/enable shader program
 	# handle errors wisely
-	return program
+	return program, program2
 
-def enable():
-	global program
+def enable(select):
 	try:
-	    glUseProgram(program)   
+	    if (select == 1):
+	    	glUseProgram(program)  
+	    else:
+	    	glUseProgram(program2)
+	    
 	except OpenGL.error.GLError:
 	    print glGetProgramInfoLog(program)
 	    raise
